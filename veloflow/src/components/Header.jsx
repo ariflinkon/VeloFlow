28ecf6c29b05081dbf39b220e0c9699e36365353
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2025-07-09 23:25:37 +0600

    Add basic layout with header and footer components

---
+++ veloflow/src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">🚀 VeloFlow</div>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">Features</a>
+        <a href="#">Pricing</a>
+        <a href="#">Login</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
