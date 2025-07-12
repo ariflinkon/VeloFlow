943bee1f289c89e972d59357876e00748602a766
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2025-07-12 23:45:29 +0600

    Added faq components

---
+++ veloflow/src/components/FAQ.jsx
@@ -0,0 +1,16 @@
+function FAQ() {
+  return (
+    <section className="faq">
+      <h2>Frequently Asked Questions</h2>
+      <div>
+        <h4>Is VeloFlow free to use?</h4>
+        <p>Yes! You can use the Free tier to build and deploy your project.</p>
+
+        <h4>Can I customize the components?</h4>
+        <p>Absolutely. Everything is modular and easily modifiable.</p>
+      </div>
+    </section>
+  );
+}
+
+export default FAQ;
